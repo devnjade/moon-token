bf55306007c5c71e4d05aed1650402fae1bf0746
Adebayo Temitope <adecancode@gmail.com>
2022-07-18 23:18:51 +0100

    update

--- contracts/Moon.sol
+++ contracts/Moon.sol
@@ -2,8 +2,17 @@
 pragma solidity ^0.8.9;
 
 // Openzeppelin contracts
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Moon {}
+contract YakToken is Ownable, ERC20, ReentrancyGuard {
+    uint256 public initTotalSupply;
+
+    constructor() ERC20("Moon", "MUN") {}
+
+    function mint(uint256 amount) public onlyOwner nonReentrant {
+        initTotalSupply = amount;
+        _mint(msg.sender, initTotalSupply * 1e18);
+    }
+}
